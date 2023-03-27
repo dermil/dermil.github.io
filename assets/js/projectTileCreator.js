b7dd05aa61b4cb0461d32e7b79b4b716a14b9b1c
Dermil Dusauzay <ddermil@yahoo.com>
2023-03-27 05:08:05 -0400

    Had to add the data directly to the .js file

--- assets/js/projectTileCreator.js
+++ assets/js/projectTileCreator.js
@@ -1,46 +1,94 @@
-import { projectDetails } from "./data";
+const projectDetails = [
+    /* template setup
+    {
+        id: 0,
+        category: "site",
+        img: "", 
+        title: "",
+        description: '',
+        link: '', //link to where the project is hosted
+        styleNumber: ''//the founding project for this has various styles that seem to change the color so i guess pick one or make it a random range
+    }
+    */
+    {
+        id: 1,
+        category: "site",
+        img: "images/manual.png", 
+        title: "Technical Documentation",
+        description: 'A mock-up Technical Documentation manual made from scratch with CSS and HTML.',
+        link: 'https://codepen.io/dermil/full/powGNqb', 
+        styleNumber: '1'
+    },
+    {
+        id: 2,
+        category: "Site",
+        img: "images/productlanding.png", 
+        title: "Product Landing Page",
+        description: 'A simple, reactive Product Landing Page mock-up made with CSS and HTML.',
+        link: 'https://codepen.io/dermil/full/WNORWYM', 
+        styleNumber: '2'
+    },
+    {
+        id: 3,
+        category: "site",
+        img: "images/survey.png", 
+        title: "Hobbies Survey Form",
+        description: "A quick and easy survey form about people's hobbies.",
+        link: 'https://codepen.io/dermil/full/zYzYBmj', 
+        styleNumber: '3'
+    },
+    {
+        id: 4,
+        category: "site",
+        img: "images/tribute.png", 
+        title: "History of Videogames",
+        description: 'An abridged History of Modern videogaming!',
+        link: 'https://codepen.io/dermil/full/JjNqVey', 
+        styleNumber: '4'
+    }
+    
+]
+'use strict';
+const e = React.createElement;
 
 class ProjectTile extends React.Component {   
+    constructor(props){
+        super(props)
+    }
+
     render() {
+        const {img, styleNumber, title, link, description} = this.props.productInfo;
         return (
-            <article class={"style"+this.props.style}>
+            <article class={"style"+styleNumber}>
                 <span class="image">
-                    <img src={this.props.img} alt="" />
+                    <img src={img} alt="" />
                 </span>
-                <a href={this.props.link} target="_blank">
-                    <h2>{this.props.title}</h2>
+                <a href={link} target="_blank">
+                    <h2>{title}</h2>
                     <div class="content">
-                        <p>{this.props.description}</p>
+                        <p>{description}</p>
                     </div>
                 </a>
             </article>
-        )
+        );
     }
 }
 
-export default class TileCreator extends React.Component {
-
+class TileCreator extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    
     render() {
-        let tilePad;
-        tilePad = projectDetails.map((tileObj, i, tileInfoArr) => {
-            return (
-                <ProjectTile 
-                    link={tileInfoArr[i].link}
-                    style={tileInfoArr[i].styleNumber}
-                    title={tileInfoArr[i].title}
-                    image={tileInfoArr[i].img}
-                    description={tileInfoArr[i].description}
+            return projectDetails.map((productInfo) => {
+                return <ProjectTile 
+                    productInfo={productInfo}
                 />
-            )
-        })
-
-        return <section class="tiles"> {tilePad} </section>
+            
+            });
     }
 }
 
-const root = reactDOM.createRoot(document.getElementById('tileRender'));
-root.render(
-    <React.StrictMode>
-        <TileCreator />
-    </React.StrictMode>
-)
+const domContainer = document.querySelector('.tiles');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(TileCreator));
